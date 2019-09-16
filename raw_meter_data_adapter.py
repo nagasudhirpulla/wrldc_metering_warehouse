58cdc3c3e2aea0b92cda2a75b99238e8468b4536
Nagasudhir Pulla <Nagasudhir@wrldc.local>
2019-09-16 11:34:29 +0530

    'changes'

--- raw_meter_data_adapter.py
+++ raw_meter_data_adapter.py
@@ -9,40 +9,47 @@ import psycopg2
 from warehouse_db_config import getWarehouseDbConfigDict
 import glob
 import pandas.io.sql as sqlio
+from itertools import chain
+
 
 class RawMeterDataAdapter:
-    conn = None    
+    conn = None
+
     def pushFolderDataToDb(self, folderpath='', recursive=True):
-        fileFormat = 'NPD'
-        files = [f for f in glob.glob(folderpath + "**/*." + fileFormat, recursive=recursive)]
+        fileFormats = ['NPD', 'NPE']
+        files = [[f for f in glob.glob(
+            folderpath + "**/*." + fileFormat, recursive=True)] for fileFormat in fileFormats]
+        files = list(chain.from_iterable(files))
         for filepath in files:
             self.pushFileDataToDb(filepath)
-    
+
     def pushFileDataToDb(self, filepath):
         txtFile = open(filepath, "r")
         txt = txtFile.read()
         txtFile.close()
         self.pushRawMeterDataTextToDb(txt)
-    
+
     def pushRawMeterDataTextToDb(self, txt):
         txtData = RawMeterDataParser.ParseRawMeterData(txt)
         cumData = txtData['cumData']
         blksData = txtData['blksData']
         self.pushRawCumData(cumData)
-        self.pushRawBlksData(blksData)            
-        
+        self.pushRawBlksData(blksData)
+
     def connectToDb(self):
         warehouseConfigDict = getWarehouseDbConfigDict()
         self.conn = psycopg2.connect(host=warehouseConfigDict['db_host'], dbname=warehouseConfigDict['db_name'],
-                            user=warehouseConfigDict['db_username'], password=warehouseConfigDict['db_password'])
-        
+                                     user=warehouseConfigDict['db_username'], password=warehouseConfigDict['db_password'])
+
     def disconnectDb(self):
         self.conn.close()
-        
+
     def pushRawCumData(self, cumData):
         cur = self.conn.cursor()
-        dataInsertionTuple = (cumData.data_time.strftime('%Y-%m-%d %H:%M:%S'), cumData.meter_id, float(cumData.cumm_active_energy_wh), float(cumData.cumm_reactive_energy_high_wh), float(cumData.cumm_reactive_energy_low_wh))
-        dataText = cur.mogrify('(%s,%s,%s,%s,%s)', dataInsertionTuple).decode("utf-8")
+        dataInsertionTuple = (cumData.data_time.strftime('%Y-%m-%d %H:%M:%S'), cumData.meter_id, float(
+            cumData.cumm_active_energy_wh), float(cumData.cumm_reactive_energy_high_wh), float(cumData.cumm_reactive_energy_low_wh))
+        dataText = cur.mogrify(
+            '(%s,%s,%s,%s,%s)', dataInsertionTuple).decode("utf-8")
         sqlTxt = 'INSERT INTO public.raw_meter_cum_data(\
         	data_time, meter_id, cumm_active_energy_wh, cumm_reactive_energy_high_wh, cumm_reactive_energy_low_wh)\
         	VALUES {0} on conflict (data_time, meter_id) \
@@ -53,7 +60,7 @@ class RawMeterDataAdapter:
         self.conn.commit()
         cur.close()
         print('Raw Cumulative Meter data push done')
-    
+
     def pushRawBlksData(self, blksData):
         cur = self.conn.cursor()
         # we will commit in multiples of 100 rows
@@ -65,36 +72,39 @@ class RawMeterDataAdapter:
             iteratorEndVal = rowIter+insIncr
             if iteratorEndVal >= numRows:
                 iteratorEndVal = numRows
-    
+
             # Create row tuples
             dataInsertionTuples = []
             for insRowIter in range(rowIter, iteratorEndVal):
                 dataRow = blksData[insRowIter]
-    
-                dataInsertionTuple = (dataRow.data_time.strftime('%Y-%m-%d %H:%M:%S'), dataRow.meter_id, dataRow.freq_code, float(dataRow.act_en_wh))
+
+                dataInsertionTuple = (dataRow.data_time.strftime(
+                    '%Y-%m-%d %H:%M:%S'), dataRow.meter_id, dataRow.freq_code, float(dataRow.act_en_wh))
                 dataInsertionTuples.append(dataInsertionTuple)
-    
+
             # prepare sql for insertion and execute
-            dataText = ','.join(cur.mogrify('(%s,%s,%s,%s)', row).decode("utf-8") for row in dataInsertionTuples)
+            dataText = ','.join(cur.mogrify('(%s,%s,%s,%s)', row).decode(
+                "utf-8") for row in dataInsertionTuples)
             sqlTxt = 'INSERT INTO public.raw_meter_data(\
         	data_time, meter_id, freq_code, act_en_wh)\
         	VALUES {0} on conflict (meter_id, data_time) \
             do update set freq_code = excluded.freq_code, act_en_wh = excluded.act_en_wh'.format(dataText)
             cur.execute(sqlTxt)
             self.conn.commit()
-    
+
             rowIter = iteratorEndVal
-    
+
         # close cursor and connection
         cur.close()
         print('Raw Meter data push done')
-    
+
     @staticmethod
     def getMeterRawBlksDataFromDb(meterId, fromTime, toTime):
         warehouseConfigDict = getWarehouseDbConfigDict()
         conn = psycopg2.connect(host=warehouseConfigDict['db_host'], dbname=warehouseConfigDict['db_name'],
-                            user=warehouseConfigDict['db_username'], password=warehouseConfigDict['db_password'])
-        sql = "select * from public.raw_meter_data where meter_id = '{0}' and data_time between '{1}' and '{2}'".format(meterId, fromTime.strftime('%Y-%m-%d %H:%M:%S'), toTime.strftime('%Y-%m-%d %H:%M:%S'))
+                                user=warehouseConfigDict['db_username'], password=warehouseConfigDict['db_password'])
+        sql = "select * from public.raw_meter_data where meter_id = '{0}' and data_time between '{1}' and '{2}'".format(
+            meterId, fromTime.strftime('%Y-%m-%d %H:%M:%S'), toTime.strftime('%Y-%m-%d %H:%M:%S'))
         df = sqlio.read_sql_query(sql, conn, index_col='id')
         conn = None
         return df
--- raw_meter_data_classes.py
+++ raw_meter_data_classes.py
@@ -18,19 +18,21 @@ hour_of_day, blk1_freq_code, blk1_active energy(Watt Hour), … till block 4
 
 import datetime as dt
 
+
 class RawMeterCumData:
     data_time = None
     meter_id = None
     cumm_active_energy_wh = None
     cumm_reactive_energy_high_wh = None
     cumm_reactive_energy_low_wh = None
+
     def __init__(self, data_time, meter_id, cumm_active_energy_wh, cumm_reactive_energy_high_wh, cumm_reactive_energy_low_wh):
         self.meter_id = meter_id
         self.cumm_active_energy_wh = cumm_active_energy_wh
         self.cumm_reactive_energy_high_wh = cumm_reactive_energy_high_wh
         self.cumm_reactive_energy_low_wh = cumm_reactive_energy_low_wh
         self.data_time = data_time
-    
+
     @staticmethod
     def parse(txtLine):
         strs = txtLine.split()
@@ -41,24 +43,27 @@ class RawMeterCumData:
         cumm_active_energy_wh = float(strs[1])
         cumm_reactive_energy_high_wh = float(strs[2])
         cumm_reactive_energy_low_wh = float(strs[3])
-        data_time = dt.datetime.strptime(strs[4], '%m-%d-%y').date()
-        meterHeader = RawMeterCumData(data_time, meter_id, cumm_active_energy_wh, cumm_reactive_energy_high_wh, cumm_reactive_energy_low_wh)
+        data_time = dt.datetime.strptime(strs[4], '%d-%m-%y').date()
+        meterHeader = RawMeterCumData(data_time, meter_id, cumm_active_energy_wh,
+                                      cumm_reactive_energy_high_wh, cumm_reactive_energy_low_wh)
         return meterHeader
-    
+
     def dict(self):
         return self.__dict__
+
 
 class RawMeterData:
     data_time = None
     meter_id = None
     freq_code = None
     act_en_wh = None
+
     def __init__(self, data_time, meter_id, freq_code, act_en_wh):
         self.data_time = data_time
         self.meter_id = meter_id
         self.freq_code = freq_code
         self.act_en_wh = act_en_wh
-    
+
     @staticmethod
     def parse(date_obj, meter_id, txtLine):
         strs = txtLine.split()
@@ -66,17 +71,23 @@ class RawMeterData:
         if len(strs) != 9:
             return None
         hr = int(strs[0])
-        blkFreqCodes = [int(strs[1]), int(strs[3]), int(strs[5]), int(strs[7])]
-        blkActEns = [float(strs[2]), float(strs[4]), float(strs[6]), float(strs[8])]
+        blkFreqCodes = [strs[1], strs[3], strs[5], strs[7]]
+        blkFreqCodes = [int(f.replace('rr', '').replace('aa', ''))
+                        for f in blkFreqCodes]
+        blkActEns = [strs[2], strs[4], strs[6], strs[8]]
+        blkActEns = [float(f.replace('*', '')) for f in blkActEns]
         dataRes = []
         for blkNum in range(0, 4):
-            data_time = dt.datetime(date_obj.year, date_obj.month, date_obj.day, hr, 0, 0) + dt.timedelta(minutes=15*blkNum)
-            blkData = RawMeterData(data_time, meter_id, blkFreqCodes[blkNum], blkActEns[blkNum])
+            data_time = dt.datetime(date_obj.year, date_obj.month,
+                                    date_obj.day, hr, 0, 0) + dt.timedelta(minutes=15*blkNum)
+            blkData = RawMeterData(data_time, meter_id,
+                                   blkFreqCodes[blkNum], blkActEns[blkNum])
             dataRes.append(blkData)
         return dataRes
-    
+
     def dict(self):
         return self.__dict__
+
 
 class RawMeterDataParser:
     @staticmethod
@@ -91,6 +102,7 @@ class RawMeterDataParser:
         dateObj = dayInfo.data_time
         meterId = dayInfo.meter_id
         blksInfo = []
-        for lineIter in range(1,25):
-            blksInfo = blksInfo + RawMeterData.parse(dateObj, meterId, txtLines[lineIter])
+        for lineIter in range(1, 25):
+            blksInfo = blksInfo + \
+                RawMeterData.parse(dateObj, meterId, txtLines[lineIter])
         return dict(cumData=dayInfo, blksData=blksInfo)
