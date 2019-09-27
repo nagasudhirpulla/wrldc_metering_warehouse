86aea9046cc4b6949af487fa3807479a27de79ab
Nagasudhir Pulla <Nagasudhir@wrldc.local>
2019-09-27 07:54:08 +0530

    changes

--- fict_location_energy_db_update_script.py
+++ fict_location_energy_db_update_script.py
@@ -7,10 +7,12 @@ Created on Sat Sep 14 10:50:06 2019
 
 from fict_location_energy_adapter import FictLocationEnergyAdapter
 import datetime as dt
-fromTime = dt.datetime(2019, 9, 7)
-toTime = dt.datetime(2019, 9, 8)
+fromTime = dt.datetime(2017, 1, 1)
+toTime = dt.datetime(2018, 1, 1)
 locEn = FictLocationEnergyAdapter()
 locEn.connectToDb()
 # locEn.deriveFictLocationEnergyForDates(fromTime, toTime, [])
-locEn.pushFolderDataToDb(folderpath='\\\\10.2.100.80\\d\\SEMBASE')
+for dayOffset in range((toTime-fromTime).days):
+    dayStr = (fromTime+dt.timedelta(days=dayOffset)).strftime('%d%m%y')
+    locEn.pushFolderDataToDb(folderpath='\\\\10.2.100.80\\d\\SEMBASE\\'+dayStr)
 locEn.disconnectDb()
